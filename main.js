7d148227a11ca0312026887ac60253fc4202c1ae
Chance1080 <542553803@qq.com>
2026-06-26 14:16:23 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll",()=>{
+
+const header=document.querySelector("header");
+
+if(window.scrollY>50){
+
+header.style.background="rgba(0,0,0,.75)";
+
+}else{
+
+header.style.background="rgba(0,0,0,.25)";
+
+}
+
+});
